43b7e88250f881aba0f1e3f732bc7c48a110f830
Dean Gardiner <me@dgardiner.net>
2016-05-10 01:15:07 +1200

    Implemented `IndexFileStorage.get()` method

--- oem_storage_file/index.py
+++ oem_storage_file/index.py
@@ -5,6 +5,7 @@ from oem_storage_file.core.base import BaseFileStorage
 from oem_storage_file.metadata import MetadataFileStorage
 
 import os
+import sys
 
 
 class IndexFileStorage(IndexStorage, BaseFileStorage, Plugin):
@@ -28,6 +29,23 @@ class IndexFileStorage(IndexStorage, BaseFileStorage, Plugin):
 
         self.path = os.path.join(self.parent.path, 'index.%s' % self.format.__extension__)
 
+    def get(self, index, key):
+        try:
+            value = index.items[str(key)]
+        except KeyError:
+            exc_info = sys.exc_info()
+
+            try:
+                value = index.items[key]
+            except KeyError:
+                raise exc_info[0], exc_info[1], exc_info[2]
+
+        # Ensure item has been parsed
+        if type(value) is dict:
+            value = index.items[str(key)] = self.parse(index.collection, key, value)
+
+        return value
+
     def load(self, collection):
         return self.main.format.from_path(
             collection, ModelRegistry['Index'], self.path,
@@ -35,7 +53,7 @@ class IndexFileStorage(IndexStorage, BaseFileStorage, Plugin):
             storage=self
         )
 
-    def parse_metadata(self, collection, key, value):
+    def parse(self, collection, key, value):
         return self.main.format.from_dict(
             collection, ModelRegistry['Metadata'], value,
             key=str(key),
